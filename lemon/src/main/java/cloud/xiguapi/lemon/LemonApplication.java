dc2a5ac7a511c1027de0d840d1b693aaebb2ad30
adanz1015 <adanz1015@outlook.com>
2020-07-25 23:29:43 +0800

    后端项目初始化。

---
+++ lemon/src/main/java/cloud/xiguapi/lemon/LemonApplication.java
@@ -0,0 +1,16 @@
+package cloud.xiguapi.lemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author adanz
+ */
+@SpringBootApplication
+public class LemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemonApplication.class, args);
+	}
+
+}
---
+++ lemon/src/test/java/cloud/xiguapi/lemon/LemonApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xiguapi.lemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
