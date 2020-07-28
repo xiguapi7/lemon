dddfbf1e7470328eafda3e4c89e93594f7bef942
adanz1015 <adanz1015@outlook.com>
2020-07-28 20:26:33 +0800

    Kaptcha验证码引入

---
+++ lemon-admin/src/main/java/cloud/xiguapi/lemon/admin/config/KaptchaConfig.java
@@ -0,0 +1,32 @@
+package cloud.xiguapi.lemon.admin.config;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * 验证码配置类
+ *
+ * @author 大大大西西瓜皮🍉
+ * @date 13:50 2020-07-28
+ * description:
+ */
+@Configuration
+public class KaptchaConfig {
+
+	@Bean
+	public DefaultKaptcha producer() {
+		Properties properties = new Properties();
+		properties.put("kaptcha.border", "no");
+		properties.put("kaptcha.textproducer.font.color", "black");
+		properties.put("kaptcha.textproducer.char.space", "5");
+
+		Config config = new Config(properties);
+		DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+		defaultKaptcha.setConfig(config);
+		return defaultKaptcha;
+	}
+}
