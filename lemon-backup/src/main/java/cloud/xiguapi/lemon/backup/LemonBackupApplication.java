3ade8622fecffa952758682a31c1bdccdfb8f557
adanz1015 <adanz1015@outlook.com>
2020-07-31 17:43:25 +0800

    添加注册中心支持.

--- lemon-backup/src/main/java/cloud/xiguapi/lemon/backup/LemonBackupApplication.java
+++ lemon-backup/src/main/java/cloud/xiguapi/lemon/backup/LemonBackupApplication.java
@@ -2,8 +2,10 @@ package cloud.xiguapi.lemon.backup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"cloud.xiguapi.lemon"})
+@EnableDiscoveryClient
 public class LemonBackupApplication {
 
 	public static void main(String[] args) {
